a9b467cd638aacbbeb05b1ec7ced80b2070191ac
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-07-08 00:33:32 +0530

    Modified: added check option

--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -2,23 +2,26 @@ package com.bridgelabz.SnakeAndLadder;
 
 public class SnakeAndLadder {
 
-    static int START_POSITION = 0;
+    static final int START_POSITION = 0;
     static final int NO_PLAY = 0;
     static final int IF_LADDER = 1;
     static final int IF_SNAKE =2;
+    static int currentPosition = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and ladder game");
         int dieNum = (int)(Math.random()*10)%6+1;
         int option = (int)(Math.random()*10)%3;
         if (option == IF_LADDER){
-            START_POSITION+=dieNum;
+            currentPosition+=dieNum;
         }
         else if (option==IF_SNAKE) {
-            START_POSITION-=dieNum;
+            currentPosition-=dieNum;
+        } else if (option==NO_PLAY) {
+            System.out.println("No Play");
         }
 
-        System.out.println(START_POSITION);
+        System.out.println(currentPosition);
 
     }
 }

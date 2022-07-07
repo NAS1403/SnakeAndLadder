9fc97e9aaf0271879b57b270201cf2f7535bb99f
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-07-07 14:56:48 +0530

    Modified:Added Random to get dice number

--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -5,5 +5,7 @@ public class SnakeAndLadder {
     static final int START_POSITION = 0;
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and ladder game");
+        int num = (int)(Math.random()*10)%6+1;
+        
     }
 }

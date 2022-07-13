86e4aa49c20760f5e4cee8d60e93331a5ffe18f2
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-07-13 13:43:42 +0530

    Added computer player

--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -5,45 +5,69 @@ public class SnakeAndLadder {
     static final int IF_LADDER = 1;
     static final int IF_SNAKE =2;
     static final int MAX_POSITION = 100;
-    static int currentPosition=0;
+    static int player1Position=0;
+    static int player2Position=0;
     static int die =0;
-    static int player=1;
 
-    static void switchPlayer(){
-        if (player==1){
-            player=2;
+    public enum CurrentPlayer{
+        PLAYER,COMPUTER
+    }
+    static CurrentPlayer toss(){
+        if((int)(Math.random()*10)%2 == 1){
+            return CurrentPlayer.PLAYER;
         }
         else{
-            player=1;
+            return CurrentPlayer.COMPUTER;
         }
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and ladder game");
-        while(currentPosition<100){
-            switchPlayer();
-
+        CurrentPlayer currentplayer = toss();
+        while(player1Position<100 && player2Position<100){
         int dieNum = (int)(Math.random()*10)%6+1;
         int option = (int)(Math.random()*10)%3;
-            if (option == IF_LADDER){
-                currentPosition+=dieNum;
-                switchPlayer();
-               }
-            else if (option==IF_SNAKE) {
-                currentPosition-=dieNum;
-            }
-            else if(option == NO_PLAY){
-                System.out.println("No Play");
-            }
-            if( currentPosition<START_POSITION){
-                currentPosition=0;
-            }
-            if(currentPosition>MAX_POSITION){
-                currentPosition-=dieNum;
-            }
-            System.out.println("Current position is: "+currentPosition);
+
+        switch(option){
+                case IF_LADDER:
+                    if (currentplayer == CurrentPlayer.PLAYER)
+                    {
+                        player1Position += dieNum;
+                        if (player1Position > MAX_POSITION) {
+                            player1Position -= dieNum;
+                        }
+                    }
+                    else {
+                        player2Position += dieNum;
+                        if (player2Position > MAX_POSITION) {
+                            player2Position -= dieNum;
+                        }
+                    }
+                    break;
+                    case IF_SNAKE:
+                            if (currentplayer == CurrentPlayer.PLAYER) {
+                                player1Position -= dieNum;
+                                if (player1Position < START_POSITION) {
+                                    player1Position = 0;
+                                }
+                            }
+                            else {
+                                player2Position -= dieNum;
+                                if (player2Position < START_POSITION) {
+                                    player2Position = 0;
+                                }
+                            }
+                            currentplayer=(currentplayer==CurrentPlayer.PLAYER)?CurrentPlayer.COMPUTER:CurrentPlayer.PLAYER;
+                            break;
+            case NO_PLAY:
+                currentplayer=(currentplayer==CurrentPlayer.PLAYER)?CurrentPlayer.COMPUTER:CurrentPlayer.PLAYER;
+                break;
+            default:
+        }
+            System.out.println("Current position of player is: "+player1Position);
+            System.out.println("Current position of computer is: "+player2Position);
             die++;
         }
         System.out.println("Total number of times Die rolled are: "+die);
-        System.out.println("Player "+ player + " wins the game");
+        System.out.println(currentplayer + " wins the game");
     }
 }
